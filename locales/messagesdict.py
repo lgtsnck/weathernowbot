fe0cab0e50a527ed0e6fc911388a24bc87aa0d03
lgtsnck <aleksey.badanin777@gmail.com>
2021-06-09 21:53:26 +0400

    added messages dict

---
+++ locales/messagesdict.py
@@ -0,0 +1,19 @@
+from locales import en, ru
+
+
+messages = {
+    "ru": {
+        "greeting": ru.GREETING,
+        "help_msg": ru.HELP_MESSAGE,
+        "forecast": ru.FORECAST,
+        "unknown_cmd": ru.UNKNOWN_COMMAND,
+        "lang_success": ru.LANG_SELECT_SUCCESS,
+    },
+    "en": {
+        "greeting": en.GREETING,
+        "help_msg": en.HELP_MESSAGE,
+        "forecast": en.FORECAST,
+        "unknown_cmd": en.UNKNOWN_COMMAND,
+        "lang_success": en.LANG_SELECT_SUCCESS,
+    },
+}
